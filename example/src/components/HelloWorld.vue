7386fb5ea4de4e0976392410f57f6c336eeab2b0
hung phan <hung.phan@episerver.com>
2018-12-28 16:57:37 +0700

    first runnable project

---
+++ example/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    test test test
+    <TodoApp/>
+  </div>
+  
+</template>
+
+<script>
+import  TodoApp  from './TodoApp.vue';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components:{
+    TodoApp: TodoApp
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ example/src/components/TodoApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in todoList" :key="item.id">
+                {{item.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TodoApp',
+        data() {
+            return { title: "Test", todoList: [{"id":"1", "name" : "Hung"}, {"id":"2", "name" : "Hieu"}]}
+        },
+
+        methods: {
+            updateData() {
+                //    this.title = 'test';
+                this.todoList =  [{"id":"1", "name" : "Hung Phan"}, {"id":"2", "name" : "Hieu Phan"}];
+            }
+        }
+    }
+</script>
